61e4ebba4685f3c8a1e91e91b7c97421d5ba353e
shotastage <hornet.live.mf@gmail.com>
2018-12-20 09:52:54 +0900

    Initial Commit

---
+++ PkgProtect/PkgProtect.h
@@ -0,0 +1,19 @@
+//
+//  PkgProtect.h
+//  PkgProtect
+//
+//  Created by Shota Shimazu on 2018/12/20.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PkgProtect.
+FOUNDATION_EXPORT double PkgProtectVersionNumber;
+
+//! Project version string for PkgProtect.
+FOUNDATION_EXPORT const unsigned char PkgProtectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PkgProtect/PublicHeader.h>
+
+
